9083a551798eb75ee794d6f5549c4f9a550af675
mauro.ojeda.f <mauro.ojeda.f@gmail.com>
2023-05-25 20:43:42 -0400

    first commit

---
+++ AlgoATexto.py
@@ -0,0 +1,397 @@
+from pandas import to_datetime
+from numpy_financial import irr 
+
+class PlataATexto:
+    #Muy basado en https://github.com/wzorroman/numbers_to_letters/blob/master/numbers_to_letters.py
+    def __init__(self, MONEDA_SINGULAR, MONEDA_PLURAL, SIGLA):
+        self.MONEDA_SINGULAR = MONEDA_SINGULAR
+        self.MONEDA_PLURAL = MONEDA_PLURAL
+        self.SIGLA = SIGLA
+
+
+    CENTIMOS_SINGULAR = 'CTV'
+    CENTIMOS_PLURAL = 'CTVS'
+
+    MAX_NUMERO = 999999999999
+
+    UNIDADES = (
+        'CERO',
+        'UNO',
+        'DOS',
+        'TRES',
+        'CUATRO',
+        'CINCO',
+        'SEIS',
+        'SIETE',
+        'OCHO',
+        'NUEVE'
+    )
+
+    DECENAS = (
+        'DIEZ',
+        'ONCE',
+        'DOCE',
+        'TRECE',
+        'CATORCE',
+        'QUINCE',
+        'DIECISEIS',
+        'DIECISIETE',
+        'DIECIOCHO',
+        'DIECINUEVE'
+    )
+
+    DIEZ_DIEZ = (
+        'CERO',
+        'DIEZ',
+        'VEINTE',
+        'TREINTA',
+        'CUARENTA',
+        'CINCUENTA',
+        'SESENTA',
+        'SETENTA',
+        'OCHENTA',
+        'NOVENTA'
+    )
+
+    CIENTOS = (
+        '_',
+        'CIENTO',
+        'DOSCIENTOS',
+        'TRESCIENTOS',
+        'CUATROSCIENTOS',
+        'QUINIENTOS',
+        'SEISCIENTOS',
+        'SETECIENTOS',
+        'OCHOCIENTOS',
+        'NOVECIENTOS'
+    )
+
+
+    def numero_a_letras(self, numero):
+        numero_entero = int(numero)
+        if numero_entero > self.MAX_NUMERO:
+            raise OverflowError('Número demasiado alto')
+        if numero_entero < 0:
+            negativo_letras = self.numero_a_letras(abs(numero))
+            return f"MENOS {negativo_letras}"
+        letras_decimal = ''
+        parte_decimal = int(round((abs(numero) - abs(numero_entero)) * 100))
+        if parte_decimal > 9:
+            letras_decimal = f"PUNTO {self.numero_a_letras(parte_decimal)}"
+        elif parte_decimal > 0:
+            letras_decimal = f"PUNTO CERO {self.numero_a_letras(parte_decimal)}"
+        if numero_entero <= 99:
+            resultado = self.leer_decenas(numero_entero)
+        elif numero_entero <= 999:
+            resultado = self.leer_centenas(numero_entero)
+        elif numero_entero <= 999999:
+            resultado = self.leer_miles(numero_entero)
+        elif numero_entero <= 999999999:
+            resultado = self.leer_millones(numero_entero)
+        else:
+            resultado = self.leer_millardos(numero_entero)
+        resultado = resultado.replace('UNO MIL', 'UN MIL')
+        resultado = resultado.strip()
+        resultado = resultado.replace(' _ ', ' ')
+        resultado = resultado.replace('  ', ' ')
+        if parte_decimal > 0:
+            resultado = f"{resultado} {letras_decimal}"
+        return resultado
+
+
+    def numero_a_moneda(self, numero):
+        numero_entero = int(numero)
+        parte_decimal = int(round((abs(numero) - abs(numero_entero)) * 100))
+        centimos = self.CENTIMOS_SINGULAR if parte_decimal == 1 else self.CENTIMOS_PLURAL
+        moneda = self.MONEDA_SINGULAR if numero_entero == 1 else self.MONEDA_PLURAL
+        letras = self.numero_a_letras(numero_entero)
+        letras = letras.replace('UNO', 'UN')
+        aux_decimal = self.numero_a_letras(parte_decimal).replace('UNO', 'UN')
+        letras_decimal = f"con {aux_decimal} {centimos}"
+        letras = f"{letras} {letras_decimal} {moneda}"
+        return letras
+
+
+    def leer_decenas(self, numero):
+        if numero < 10:
+            return self.UNIDADES[numero]
+        decena, unidad = divmod(numero, 10)
+        if numero <= 19:
+            resultado = self.DECENAS[unidad]
+        elif numero <= 29:
+            resultado = f"VEINTI{self.UNIDADES[unidad]}"
+            if resultado == "VEINTICERO":
+                resultado = "VEINTE"
+        else:
+            resultado = self.DIEZ_DIEZ[decena]
+            if unidad > 0:
+                resultado = f"{resultado} Y {self.UNIDADES[unidad]}"
+        return resultado
+
+
+    def leer_centenas(self, numero):
+        centena, decena = divmod(numero, 100)
+        if numero == 0:
+            resultado = 'CIEN'
+        else:
+            resultado = self.CIENTOS[centena]
+            if decena > 0:
+                decena_letras = self.leer_decenas(decena)
+                resultado = f"{resultado} {decena_letras}"
+            if resultado == "CIENTO":
+                resultado = "CIEN"
+        return resultado
+
+
+    def leer_miles(self,numero):
+        millar, centena = divmod(numero, 1000)
+        resultado = ''
+        if millar == 1:
+            resultado = ''
+        if (millar >= 2) and (millar <= 9):
+            resultado = self.UNIDADES[millar]
+        elif (millar >= 10) and (millar <= 99):
+            resultado = self.leer_decenas(millar)
+        elif (millar >= 100) and (millar <= 999):
+            resultado = self.leer_centenas(millar)
+        resultado = f"{resultado} MIL"
+        if centena > 0:
+            centena_letras = self.leer_centenas(centena)
+            resultado = f"{resultado} {centena_letras}"
+        return resultado.strip()
+
+
+    def leer_millones(self, numero):
+        millon, millar = divmod(numero, 1000000)
+        resultado = ''
+        if millon == 1:
+            resultado = ' UN MILLON '
+        if (millon >= 2) and (millon <= 9):
+            resultado = self.UNIDADES[millon]
+        elif (millon >= 10) and (millon <= 99):
+            resultado = self.leer_decenas(millon)
+        elif (millon >= 100) and (millon <= 999):
+            resultado = self.leer_centenas(millon)
+        if millon > 1:
+            resultado = f"{resultado} MILLONES"
+        if (millar > 0) and (millar <= 999):
+            centena_letras = self.leer_centenas(millar)
+            resultado = f"{resultado} {centena_letras}"
+        elif (millar >= 1000) and (millar <= 999999):
+            miles_letras = self.leer_miles(millar)
+            resultado = f"{resultado} {miles_letras}"
+        return resultado
+
+
+    def leer_millardos(self, numero):
+        millardo, millon = divmod(numero, 1000000)
+        miles_letras = self.leer_miles(millardo)
+        millones_letras = self.leer_millones(millon)
+        return f"{miles_letras} MILLONES {millones_letras}"
+
+
+    def numero_a_moneda_sunat(self, numero):
+        numero_entero = int(numero)
+        parte_decimal = int(round((abs(numero) - abs(numero_entero)) * 100))
+        moneda = self.MONEDA_SINGULAR if numero_entero == 1 else self.MONEDA_PLURAL
+
+        letras = self.numero_a_letras(numero_entero)
+        letras = letras.replace('UNO', 'UN')
+        letras = f"{letras} CON {parte_decimal}/100 {moneda}"
+        return letras
+    
+    def deletrear(self, monto):
+        monto_letras = self.numero_a_moneda_sunat(monto)
+        monto_redondeado = round(monto, 2)
+        final = f"{monto_letras} ({monto_redondeado} {self.SIGLA})"
+        return final
+    
+    def tasas(self, tasa):
+        tasa = round(tasa, 2)
+        num_letras = self.numero_a_letras(tasa)
+        return f"{num_letras} POR CIENTO ({tasa}%)"
+    
+    def tasas_float(self, tasa):
+        tasa = tasa*100
+        texto = self.tasas(tasa)
+        return texto
+
+    def enteros(self, entero):
+        entero = int(entero)
+        num_letras = self.numero_a_letras(entero)
+        return f"{num_letras} ({entero})"
+
+
+class CosasATexto:
+    
+    def __init__(self):
+        self.diccionario_numeros = {
+        '0': 'CERO',
+        '1': 'UNO',
+        '2': 'DOS',
+        '3': 'TRES',
+        '4': 'CUATRO',
+        '5': 'CINCO',
+        '6': 'SEIS',
+        '7': 'SIETE',
+        '8': 'OCHO',
+        '9': 'NUEVE'
+    }
+
+    def DNI(self, dni):
+        resultado = ''
+        for caracter in dni:
+            if caracter.isdigit():
+                resultado += self.diccionario_numeros[caracter] + ', '
+            elif caracter == "-":
+                resultado += "GUION, "
+            else:
+                resultado += 'LETRA ' + caracter + ', '
+        resultado += '(' + dni + ')'
+        return resultado
+
+    def mayus(self, texto):
+        return texto.upper()
+    
+    
+    def fecha(self, fecha):
+        ts = to_datetime(str(fecha)) 
+        fecha_str = ts.strftime('%d/%m/%Y')
+
+        dic_meses = {
+            1: 'ENERO',
+            2: 'FEBRERO',
+            3: 'MARZO',
+            4: 'ABRIL',
+            5: 'MAYO',
+            6: 'JUNIO',
+            7: 'JULIO',
+            8: 'AGOSTO',
+            9: 'SEPTIEMBRE',
+            10: 'OCTUBRE',
+            11: 'NOVIEMBRE',
+            12: 'DICIEMBRE'
+        }
+        
+        dic_numeros = {
+            1: 'UNO',
+            2: 'DOS',
+            3: 'TRES',
+            4: 'CUATRO',
+            5: 'CINCO',
+            6: 'SEIS',
+            7: 'SIETE',
+            8: 'OCHO',
+            9: 'NUEVE',
+            10: 'DIEZ',
+            11: 'ONCE',
+            12: 'DOCE',
+            13: 'TRECE',
+            14: 'CATORCE',
+            15: 'QUINCE',
+            16: 'DIECISÉIS',
+            17: 'DIECISIETE',
+            18: 'DIECIOCHO',
+            19: 'DIECINUEVE',
+            20: 'VEINTE',
+            21: 'VEINTIUNO',
+            22: 'VEINTIDOS',
+            23: 'VEINTITRÉS',
+            24: 'VEINTICUATRO',
+            25: 'VEINTICINCO',
+            26: 'VEINTISÉIS',
+            27: 'VEINTISIETE',
+            28: 'VEINTIOCHO',
+            29: 'VEINTINUEVE',
+            30: 'TREINTA',
+            31: 'TREINTA Y UNO'
+        }
+        
+        dic_anio = {
+            2015: 'DOS MIL QUINCE',
+            2016: 'DOS MIL DIECISÉIS',
+            2017: 'DOS MIL DIECISIETE',
+            2018: 'DOS MIL DIECIOCHO',
+            2019: 'DOS MIL DIECINUEVE',
+            2020: 'DOS MIL VEINTE',
+            2021: 'DOS MIL VEINTIUNO',
+            2022: 'DOS MIL VEINTIDÓS',
+            2023: 'DOS MIL VEINTITRÉS',
+            2024: 'DOS MIL VEINTICUATRO',
+            2025: 'DOS MIL VEINTICINCO',
+            2026: 'DOS MIL VEINTISÉIS',
+            2027: 'DOS MIL VEINTISIETE',
+            2028: 'DOS MIL VEINTIOCHO',
+            2029: 'DOS MIL VEINTINUEVE',
+            2030: 'DOS MIL TREINTA',
+            2031: 'DOS MIL TREINTA Y UNO',
+            2032: 'DOS MIL TREINTA Y DOS',
+            2033: 'DOS MIL TREINTA Y TRES',
+            2034: 'DOS MIL TREINTA Y CUATRO',
+            2035: 'DOS MIL TREINTA Y CINCO',
+            2036: 'DOS MIL TREINTA Y SEIS',
+            2037: 'DOS MIL TREINTA Y SIETE',
+            2038: 'DOS MIL TREINTA Y OCHO',
+            2039: 'DOS MIL TREINTA Y NUEVE',
+            2040: 'DOS MIL CUARENTA',
+            2041: 'DOS MIL CUARENTA Y UNO',
+            2042: 'DOS MIL CUARENTA Y DOS',
+            2043: 'DOS MIL CUARENTA Y TRES',
+            2044: 'DOS MIL CUARENTA Y CUATRO',
+            2045: 'DOS MIL CUARENTA Y CINCO',
+            2046: 'DOS MIL CUARENTA Y SEIS',
+            2047: 'DOS MIL CUARENTA Y SIETE',
+            2048: 'DOS MIL CUARENTA Y OCHO',
+            2049: 'DOS MIL CUARENTA Y NUEVE',
+            2050: 'DOS MIL CINCUENTA'
+        }
+
+
+
+
+
+        dia, mes, anio = fecha_str.split("/")
+        dia = int(dia)
+        mes = int(mes)
+        anio = int(anio)
+        
+        fecha_palabras = dic_numeros[dia] + " DE " + dic_meses[mes] + " DEL " + dic_anio[anio]
+
+        return fecha_palabras
+
+class Financiero:
+    
+    def __init__(self):
+        pass
+
+    def flujos(self, monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision):
+        flujos = [-(monto_prestamo * (1-comision))]
+        flujos.extend([0]*int(meses_gracia))
+        flujos.extend([monto_cuotas]*int(numero_cuotas))
+        return flujos
+
+    def TIR_mensual(self, monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision):
+        tir = irr(self.flujos(monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision)) * 100
+        return round(tir, 2)
+
+    def TIR_anual(self, monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision):
+        tir = irr(self.flujos(monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision))
+        tir = (1 + tir)**12
+        tir = tir -1
+        tir = tir * 100
+        return round(tir, 2)
+
+    def tasa_mora(self, monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision):
+        tir = irr(self.flujos(monto_prestamo, meses_gracia, numero_cuotas, monto_cuotas, comision))
+        tir = (1 + tir)**12
+        tir = tir -1
+        tir = tir * 100
+        tir = tir/4
+        return round(tir, 2)
+
+Cordoba = PlataATexto("CORDOBA", "CORDOBAS", "NIO")
+Dolar = PlataATexto("DOLAR", "DOLARES", "USD")
+NumTasa = PlataATexto("", "", "")
+CosaATexto = CosasATexto()
+Fin = Financiero()
+
---
+++ Main.py
@@ -0,0 +1,157 @@
+#Variables
+nombreExcel = "DATOS CONTRATO V1.2.xlsx"
+nombreHoja = "DATOS"
+hojaFinal = 'DATOS FINALES'
+
+#Imports
+print(f"Iniciando: {nombreExcel}")
+import pandas as pd 
+import AlgoATexto 
+import os
+import sys
+
+#Cerrar el programa de cara al usuario
+def cierre():
+    print("Puedes cerrar esta ventana")
+    print("Presione cualquier tecla para cerrar...")
+    os.system("pause >nul")
+    sys.exit(0)
+
+print()
+print(f"Asegurece que el archivo '{nombreExcel}' este cerrado")
+print(f"No abra el archivo '{nombreExcel}' hasta que el proseso termine")
+print()
+print("---------------------------------------------------------")
+print()
+
+
+#Leer los datos desde el excel
+try:
+    print(f"Extrayendo informacion de la hoja '{nombreHoja}' de '{nombreExcel}'")
+    dfBase = pd.read_excel(nombreExcel, sheet_name=nombreHoja)
+    dfBase = dfBase.dropna()
+
+except FileNotFoundError as e:
+    print()
+    print("ERROR de archivo:", e)
+    print("El programa se detuvo debido a un error de busqueda.")
+    print(f"Asegurate el archivo '{nombreExcel}' se encuentre en la misma carpeta que el programa y/o el nombre este correctamente escrito.")
+    print()
+    cierre()
+
+except ValueError as e:
+    print()
+    print("ERROR de valor:", e)
+    print("El programa se detuvo debido a un error en el valor de una variable.")
+    print(f"Asegurate exista la hoja '{nombreHoja}' en el archivo '{nombreExcel}'.")
+    print()
+    cierre()
+
+
+# Crear df para guardar la informacion
+try:
+    print(f"Prosesando la informacion de {nombreExcel}")
+    df = pd.DataFrame()
+
+    # Transformar y guardar datos
+    df['ID'] = dfBase['ID']
+
+    #Datos Deudora
+    df['Nombre May.'] = dfBase['Nombre'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Apellidos May.'] = dfBase['Apellidos'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Cedula en letras'] = dfBase['Cédula'].apply(AlgoATexto.CosaATexto.DNI)
+    df['Estado civil may.'] = dfBase['Estado civil'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Direc. Domicilio May.'] = dfBase['Dirección del domicilio'].apply(AlgoATexto.CosaATexto.mayus)
+    df['MUNICIPIO'] = dfBase['Municipio'].apply(AlgoATexto.CosaATexto.mayus)
+    df['DEPARTAMENTO'] = dfBase['Departamento'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Tipo de neg. May.'] = dfBase['Tipo de negocio'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Profesión_Deudora'] = dfBase['Profesión Deudora'].apply(AlgoATexto.CosaATexto.mayus)
+
+    #Datos Fiador
+    df['Nombre y apellido fiador may'] = dfBase['Nombre y apellidos del Fiador'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Estado civil fiador may'] = dfBase['Estado civil Fiador'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Profesion fiador may'] = dfBase['Profesión Fiador'].apply(AlgoATexto.CosaATexto.mayus)
+    df['Direccion domicilio may'] = dfBase['Dirección domicilio Fiador'].apply(AlgoATexto.CosaATexto.mayus)
+    df['cedula fiad letras'] = dfBase['Cédula Fiador'].apply(AlgoATexto.CosaATexto.DNI)
+
+    #Datos Credito
+    df['Fecha cheque letras'] = dfBase['Fecha de cheque'].apply(AlgoATexto.CosaATexto.fecha)
+    df['Fecha de vencimiento letras'] = dfBase['Fecha de vencimiento'].apply(AlgoATexto.CosaATexto.fecha)
+    df['INVERSION LETRAS CORDOBAS'] = dfBase['Inversión Cordobas'].apply(AlgoATexto.Cordoba.deletrear)
+    df['Inversion en letras'] = dfBase['Inversión Dolares'].apply(AlgoATexto.Dolar.deletrear)
+    df['Cantidad a pagar en letras'] = dfBase['Cantidad total a pagar'].apply(AlgoATexto.Dolar.deletrear)
+    df['Cuotas en letras'] = dfBase['Cuotas'].apply(AlgoATexto.Dolar.deletrear)
+    df['Tasa_Nominal'] = dfBase['Tasa Nominal'].apply(AlgoATexto.NumTasa.tasas_float)
+    df['Comision_Desembolso'] = dfBase['Comision desembolso'].apply(AlgoATexto.NumTasa.tasas_float)
+
+    dfBase['TCEM'] = dfBase.apply(lambda row: AlgoATexto.Fin.TIR_mensual(row['Inversión Dolares'], row['Meses de gracia'], row['Numero Cuotas'], row['Cuotas'],row['Comision desembolso']), axis=1)
+    df['TCEM'] = dfBase['TCEM'].apply(AlgoATexto.NumTasa.tasas)
+
+    dfBase['TCEA'] = dfBase.apply(lambda row: AlgoATexto.Fin.TIR_anual(row['Inversión Dolares'], row['Meses de gracia'], row['Numero Cuotas'], row['Cuotas'],row['Comision desembolso']), axis=1)
+    df['TCEA'] = dfBase['TCEA'].apply(AlgoATexto.NumTasa.tasas)
+
+    dfBase['Tasa_Mora'] = dfBase.apply(lambda row: AlgoATexto.Fin.tasa_mora(row['Inversión Dolares'], row['Meses de gracia'], row['Numero Cuotas'], row['Cuotas'],row['Comision desembolso']), axis=1)
+    df['Tasa_Mora'] = dfBase['Tasa_Mora'].apply(AlgoATexto.NumTasa.tasas)
+
+    df['Meses_de_Gracia'] = dfBase['Meses de gracia'].apply(AlgoATexto.NumTasa.enteros)
+    df['Numero_de_Cuotas'] = dfBase['Numero Cuotas'].apply(AlgoATexto.NumTasa.enteros)
+    df['Plazo_Meses'] = dfBase.apply(lambda row: AlgoATexto.NumTasa.enteros(row["Meses de gracia"]+row["Numero Cuotas"]), axis=1)
+    df['Dia_de_Pago'] = dfBase['Dia de pago'].apply(AlgoATexto.NumTasa.enteros)
+
+except KeyError as e:
+    print()
+    print("ERROR de llave:", e)
+    print("El programa se detuvo debido a un error en el valor de una variable.")
+    print(f"Probablemente se modifico el nombre de una columna en la hoja '{nombreHoja}' en el archivo '{nombreExcel}'.")
+    print("Revierta el cambio en el nombre de la columna del excel o consiga una version del excel anterior")
+    print()
+    cierre()
+
+
+except ValueError as e:
+    print()
+    print("ERROR de lectura:", e)
+    print("El programa se detuvo debido a un error al conectarce al excel.")
+    print(f"Probablemente hiso correr el programa sin guardar el archivo '{nombreExcel}' entre las ejecuciones.")
+    print(f"Abra el archivo {nombreExcel} y guarde el contenido, luego ejecute el programa")
+    print()
+    cierre()
+    
+# Crear un objeto ExcelWriter dirigido al excel existente
+try:
+    print(f"Guardando '{hojaFinal}' en '{nombreExcel}'")
+    writer = pd.ExcelWriter(nombreExcel, engine='openpyxl', mode='a', if_sheet_exists="replace")
+
+    # Agregar los datos una nueva hoja al libro de Excel existente
+    df.to_excel(writer, sheet_name=hojaFinal, index=False)
+
+    # Ajustar el ancho de las columnas al contenido
+    ws = writer.sheets[hojaFinal]
+    for columnas in ws.columns:
+        largo = max(len(str(cell.value)) for cell in columnas)
+        ws.column_dimensions[columnas[0].column_letter].width = largo
+
+    # Guardar los cambios
+    writer.save()
+    writer.close()
+
+
+except PermissionError as e:
+    print()
+    print("ERROR de permiso:", e)
+    print("El programa se detuvo debido a un error de permiso.")
+    print(f"Asegurate que el archivo '{nombreExcel}' no este abierto al momento de ejecutar el programa.")
+    print()
+    cierre()
+
+
+print("Todo salio bien :D")
+print()
+print("---------------------------------------------------------")
+print()
+print("Siguientes pasos:")
+print(f"1) Abrir '{nombreExcel}'")
+print(f"2) Revisar que en la hoja '{hojaFinal}' este todo correcto, corregir casillas si es necesario.")
+print("3) Guardar el archivo")
+print()
+cierre()
